2132aa34bea80f82e1e04371071d82b8b3f90346
Linus Nilsson <linus.erik.nilsson@outlook.com>
2023-05-30 22:24:04 +0200

    Added Login/Logout ability

--- View/SettingsPage.xaml.cs
+++ View/SettingsPage.xaml.cs
@@ -6,4 +6,10 @@ public partial class SettingsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void LogoutCommand(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"//{nameof(MainPage)}");
+
+    }
 }
