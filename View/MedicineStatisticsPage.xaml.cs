629467e4b83f58970762e5d7528c9ca135c2dfe4
Linus Nilsson <linus.erik.nilsson@outlook.com>
2023-05-30 22:45:19 +0200

    Added basic data to statistics page

--- View/MedicineStatisticsPage.xaml.cs
+++ View/MedicineStatisticsPage.xaml.cs
@@ -1,9 +1,29 @@
+using ALMSAPP.Model;
+using System.Collections.ObjectModel;
+
 namespace ALMSAPP.View;
 
 public partial class MedicineStatisticsPage : ContentPage
 {
+	public ObservableCollection<MedicineItem> Items { get; set; } = new ObservableCollection<MedicineItem>();
+
 	public MedicineStatisticsPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
+
+	protected override void OnAppearing()
+	{
+        base.OnAppearing();
+        AddData();
+    }
+
+	public void AddData()
+	{
+		Items.Add(new MedicineItem { Name = "Paracetamol" });
+		Items.Add(new MedicineItem { Name = "Ibuprofen" });
+		Items.Add(new MedicineItem { Name = "Aspirin" });
+	}
+
 }
